512e5cd198ccfa83537a89f4d142323d5e4471ac
yuhtruong <yuhtruong1995@gmail.com>
2022-12-21 15:21:14 +0700

    done lessons micard

--- Angela-Yu-Course/micard/lib/main.dart
+++ Angela-Yu-Course/micard/lib/main.dart
@@ -1,10 +1,10 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+const mozartImageUrl = 'https://avatar-ex-swe.nixcdn.com/singer/avatar/2016/01/25/4/1/1/7/1453717818319_600.jpg';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -30,14 +30,73 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 50, horizontal: 10),
-          width: 100,
-          height: 100,
-          color: Colors.white,
-          child: Text('Hello'),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircleAvatar(
+            radius: 50.0,
+            backgroundColor: Colors.red,
+            backgroundImage: NetworkImage(mozartImageUrl),
+          ),
+          const Text(
+            'Mozart',
+            style: TextStyle(
+              fontFamily: 'Pacifico',
+              fontSize: 39,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            'Music Composer',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'Source Sans Pro',
+              color: Colors.teal.shade100,
+              letterSpacing: 2.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+            width: 150,
+            child: Divider(
+              color: Colors.teal.shade300,
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+            child: ListTile(
+              leading: const Icon(
+                Icons.piano,
+                color: Colors.teal,
+              ),
+              title: Text(
+                'A genius of musical music.',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.teal.shade900,
+                ),
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+            child: ListTile(
+              leading: const Icon(
+                Icons.flag,
+                color: Colors.teal,
+              ),
+              title: Text(
+                'Austria',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.teal.shade900,
+                  letterSpacing: 1.5,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
